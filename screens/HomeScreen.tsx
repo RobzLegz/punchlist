a764eba03d9849ef1201b8cadff347c1318e707d
Rob <robzlegz@gmail.com>
2021-12-21 20:01:12 +0200

    cool

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,10 @@
 import React from "react"
-import { View, Text } from "react-native"
 import styled from "styled-components/native";
 
 export default function HomeScreen() {
     return (
         <StyledHome>
-            <Text>hello</Text>
+            
         </StyledHome>
     )
 }
