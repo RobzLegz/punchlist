4acafdbd1bdea097f6de8fb7526c14946f181266
Roberts Legzdins <roberts.legzdins@di.lv>
2023-12-07 11:33:39 +0200

    home

--- mobile/src/components/home/HomeContainer.tsx
+++ mobile/src/components/home/HomeContainer.tsx
@@ -1,37 +1,55 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import { AppInfo, selectApp } from "../../redux/slices/appSlice";
 import { useSelector } from "react-redux";
-import CameraButton from "../camera/CameraButton";
 
 const HomeContainer = () => {
   const appInfo: AppInfo = useSelector(selectApp);
 
   return (
-    <View style={{ flex: 1, width: "100%" }}>
-      <TouchableOpacity
-        style={{
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
-        }}
-      >
-        <Text>Jauns Projekts</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1, width: "100%", position: "relative" }}>
+      <View style={{ flex: 1, width: "100%" }}>
+        {appInfo.projects.length > 0 ? (
+          <ScrollView></ScrollView>
+        ) : (
+          <View
+            style={{
+              width: "100%",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingHorizontal: 20
+            }}
+          >
+            <Text style={{ color: "gray", fontSize: 18 }}>
+              Pagaidåm nav neviena projekta
+            </Text>
+          </View>
+        )}
+      </View>
 
       <View
-        style={{
-          position: "absolute",
-          bottom: 30,
-          left: 0,
-          width: "100%",
-          alignItems: "center",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
+        style={{ width: "100%", padding: 15, position: "absolute", bottom: 0 }}
       >
-        <CameraButton />
+        <TouchableOpacity
+          style={{
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            height: 60,
+            backgroundColor: "#000",
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 16 }}>Jauns Projekts</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
